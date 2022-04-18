c8ebeafe8e08325839a9575c14b670f9aaaf0b41
Jodecir Neto <jodecirneto@gmail.com>
2022-04-18 19:45:56 -0300

    UPDATE guessing game with repeating loop & expect

--- sequencia/advinha/src/main.rs
+++ sequencia/advinha/src/main.rs
@@ -1,14 +1,36 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Advinhe o número, Digite o seu palpite:");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Falha ao ler o palpite");
+    let num_random:u32 = rand::thread_rng()
+        .gen_range(1, 101);
     
-    println!("Você palpitou: {}", guess);
+    loop {
+        let mut guess = String::new();
+        println!("Chute um número de 1 a 100:");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Falha ao ler o palpite");
+
+        let guess: u32 = match guess
+            .trim()
+            .parse()
+            {
+               Ok(num) => num,
+               Err(_) => continue,
+            };
+        
+        println!("Você palpitou: {}", guess);
+
+        match guess.cmp(&num_random) {
+            Ordering::Less => println!("O número é maior"),
+            Ordering::Greater => println!("O número é menor"),
+            Ordering::Equal => {
+                println!("Parabéns, você tem muita sorte e acertou!");
+                break;
+            }
+        }
+    }
 }
